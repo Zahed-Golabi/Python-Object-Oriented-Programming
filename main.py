5a482ee3fa27b289e980c04d69705e8ddb2c9486
Zahed Golabi <golabizahed@gmail.com>
2025-07-13 12:16:31 +0330

    create object and class variable

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 class Employee:
-    pass
+    salary = 4000  # class variable
+
+    def __init__(self, name, age):
+        self.name = name   # object variable
+        self.age = age     # object varialbe
 
 
 
 # create object
-employee1 = Employee()
-employee1.name = "Zanko"
-employee1.age = 32
+employee1 = Employee("Zanko", 32)
+
 
 # create object
-employee2 = Employee()
-employee2.name = "Mardin"
-employee2.age = 15
+employee2 = Employee("Mardin", 15)
 
 # print object 1 address
 print(employee1)
-print(f"Employee 1 name: {employee1.name}, age: {employee1.age}")
+print(f"Employee 1 name: {employee1.name}, age: {employee1.age}, last: {employee1.salary}")
 
-# print object 2
+# print object 2 address
 print(employee2)
-print(f"Employee 2 name: {employee2.name}, age: {employee2.age}")
+print(f"Employee 2 name: {employee2.name}, age: {employee2.age}, last: {employee2.salary}")
+
+print("Salary: ", Employee.salary)
+Employee.salary = Employee.salary * 3   # change class variable
+print("Salar: ", Employee.salary)
+
+print(f"Employee 1 name: {employee1.name}, age: {employee1.age}, last: {employee1.salary}")
+
+employee2.salary = 8000   # change object variable
+print(employee2.salary)
