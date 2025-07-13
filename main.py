24502f1b05664fd8076427edff2de5e94709bd4d
Zahed Golabi <golabizahed@gmail.com>
2025-07-13 10:41:33 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class Employee:
+    pass
+
+
+
+# create object
+employee1 = Employee()
+employee1.name = "Zanko"
+employee1.age = 32
+
+# create object
+employee2 = Employee()
+employee2.name = "Mardin"
+employee2.age = 15
+
+# print object 1 address
+print(employee1)
+print(f"Employee 1 name: {employee1.name}, age: {employee1.age}")
+
+# print object 2
+print(employee2)
+print(f"Employee 2 name: {employee2.name}, age: {employee2.age}")
